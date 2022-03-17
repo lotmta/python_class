07c893069e96b311e84ad141af1b711b450d8623
lot <lotmateohernandezespinosa@gmail.com>
2022-03-17 12:27:39 -0600

    Trabajo en clase

--- src/Script.py
+++
@@ -1,5 +0,0 @@
-dna = input("Introduce tu secuencia de DNA:\n")
-print("La secuencia de DNA es: ", dna)
-
-print(
-    f"Cantidad de A:{dna.count('A')} C:{dna.count('C')} G:{dna.count('G')} T:{dna.count('T')} ")
