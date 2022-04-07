f227144761fded8524246c0fa6cefda3da080861
lot <lotmateohernandezespinosa@gmail.com>
2022-04-06 22:11:19 -0500

    primer ejercicio terminado

--- src/ejercicio_eliminar_adaptador.py
+++ src/ejercicio_eliminar_adaptador.py
@@ -1,7 +1,34 @@
-file = open("data/4_input_adapters.txt")
+'''
+NAME
+	ejercicio_eliminar_adaptador.py
+    
+VERSION
+    1.0
+    
+AUTHOR
+	Lot Hernandez	
+    
+DESCRIPTION
+    Da la secuencia que esta en data/4_input_adapters.txt en la carpeta results pero sin los primeros 14 caracteres de la primera lista
+        
+CATEGORY
+	Genomic Sequence
+    
+USAGE
+'''
 
-contenido = file.read()
-lineas = contenido.split("ATTCGATTATAAGC")
+from statistics import linear_regression
+
+# Se abre el archivo y se separa por lineas en una lista
+file = open("data/4_input_adapters.txt")
+lineas = file.readlines()
+
+# Se abre el archivo output
+output = open("results/secuencia_sin_adaptador.txt", "w")
+
+# Se cierra el archivo para que no se quede en la memoria
 file.close()
+
+# Se abre el for donde por cada linea del input se va a escribir esa misma linea en el output pero sin los primeros 14 caracteres
 for linea in lineas:
-    print(linea)
+    output.write(f"{linea[14:]}")
