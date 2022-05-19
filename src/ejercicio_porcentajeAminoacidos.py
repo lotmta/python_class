0aeff77484c6ad500ad300e656dbeffcda6ab73d
lot <lotmateohernandezespinosa@gmail.com>
2022-05-18 20:50:47 -0500

    porcentaje aminoacidos terminado sin funcion lista

--- src/ejercicio_porcentajeAminoacidos.py
+++ src/ejercicio_porcentajeAminoacidos.py
@@ -1,6 +1,49 @@
+'''
+NAME
+	ejercicio_porcentajeAminoacidos.py
+    
+VERSION
+    1.0
+    
+AUTHOR
+	Lot Hernandez	
+    
+DESCRIPTION
+    Da el porcentaje de un cierto aminoacido de una secuencia dada
+        
+CATEGORY
+	Genomic Sequence
+    
+USAGE
+'''
 
-#sec = input("Introduce tu secuencia de aminoacidos: ")
-#amin = input("De que aminoacido quieres buscar el porcentaje?: ")
+import argparse
+
+parser = argparse.ArgumentParser(
+    description="Da el porcentaje de un aminoacido especifico de una secuencia dada")
+
+parser.add_argument("-sec", "--secuencia",
+                    help="Tu secuencia",
+                    required=True)
+
+parser.add_argument("-a", "--aminoacido",
+                    help="El aminoacido del que quieres saber el porcentaje",
+                    required=True)
+args = parser.parse_args()
+
+# Se meten los argumentos a variables
+sec = args.secuencia
+amin = args.aminoacido
+
+
+# Se checa que la longitud de secuencia no sea 0 y que solo se haya pedido el porcentaje de 1 aminoacido
+if(len(sec) == 0):
+    print("Error: No introdujiste una secuencia")
+    quit()
+if(len(amin) != 1):
+    print("Error: Solo introduce un aminoacido")
+    quit()
+
 
 # Funcion con la que se saca el porcentaje
 def porcentajeamin(sec, amin):
@@ -8,9 +51,5 @@ def porcentajeamin(sec, amin):
     return(porcentaje)
 
 
-assert porcentajeamin("MSRSLLLRFLLFLLLLPPLP", "M") == 5
-assert porcentajeamin("MSRSLLLRFLLFLLLLPPLP", "r") == 10
-assert porcentajeamin("msrslllrfllfllllpplp", "L") == 50
-assert porcentajeamin("MSRSLLLRFLLFLLLLPPLP", "Y") == 0
-
-#porcentajeamin(sec, amin)
+print(
+    f"El porcentaje de {amin} en tu secuencia es:\n{porcentajeamin(sec, amin)} %")
