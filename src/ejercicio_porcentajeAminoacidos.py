fd13f3eacc8564e990613583e74ca2fb5ab113d7
lot <lotmateohernandezespinosa@gmail.com>
2022-05-18 21:24:55 -0500

    porcentaje aminoacidos lista terminado

--- src/ejercicio_porcentajeAminoacidos.py
+++ src/ejercicio_porcentajeAminoacidos.py
@@ -32,7 +32,11 @@ args = parser.parse_args()
 
 # Se meten los argumentos a variables
 sec = args.secuencia
-amin = list(args.lista.rstrip(","))
+amin = args.lista
+
+# Se checa que se haya metido algo como parametro -l
+if(amin != None):
+    amin = list(amin.rstrip(","))
 
 
 # Se checa que la longitud de secuencia no sea 0 y que solo se haya pedido el porcentaje de 1 aminoacido
@@ -53,5 +57,10 @@ def porcentajeamin(sec, amin=['A', 'I', 'L', 'M', 'F', 'W', 'Y', 'V']):
     return(porcentaje)
 
 
-print(
-    f"El porcentaje en tu secuencia es:\n{porcentajeamin(sec, amin)} %")
+# Se checa que se haya metido algo como parametro -l
+if(amin != None):
+    print(
+        f"El porcentaje de aminoacidos de tu lista en tu secuencia es:\n{porcentajeamin(sec, amin)} %")
+else:
+    print(
+        f"El porcentaje de aminoacidos hidrofilicos en tu secuencia es:\n{porcentajeamin(sec)} %")
