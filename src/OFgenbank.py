d5b9816b3b49a1fc93a342c754776bb433550114
lot <lotmt@outlook.com>
2022-10-03 20:21:29 -0500

    Act

--- src/OFgenbank.py
+++ src/OFgenbank.py
@@ -3,13 +3,13 @@ NAME
 	OFgenbank.py
     
 VERSION
-    1.0
+    1.1
     
 AUTHOR
 	Lot Hernandez	
     
 DESCRIPTION
-    Da las fechas y nombres de los organismos de un archivo genbank
+    Da las fechas, nombres y pais de los organismos de un archivo genbank
 
 CATEGORY
 	Genomic Sequence
@@ -34,11 +34,11 @@ args = parser.parse_args()
 
 
 
-# Se imprime la fecha y los nombres de los organismos en el archivo
+# Se imprime la fecha, los nombres y el pais de los organismos en el archivo
 for gb in SeqIO.parse(args.file, 'genbank'):
     print(f"Organismo: {gb.annotations['organism']}")
     print(f"Fecha: {gb.annotations['date']}")
-
+    print(f"Pais: {gb.features[0].qualifiers['country']}")
 
 
 
